923d2ffdef84ff15fdfbb67f870ce043200a1484
Jean-Philippe Encausse <Jp@encausse.net>
2014-01-07 22:05:01 +0100

    Fix Meteo URL using JSON
    There is still the old code with scraping sample

--- meteo.js
+++ meteo.js
@@ -8,8 +8,9 @@ var action = function(data, callback, config, SARAH){
     return;
   }
   
-  var url = 'http://mobile.meteofrance.com/france/ville/'+(data.zip || config.zip);
+  var url = 'http://www.meteo-france.mobi/ws/getDetail/france/'+(data.zip || config.zip)+'.json'
   var request = require('request');
+  
   request({ 'uri' : url }, function (err, response, body){
   
     if (err || response.statusCode != 200) {
@@ -43,20 +44,50 @@ exports.init = function(bot){ SARAH = bot; }
 // ==========================================
 
 var scrap = function(body, date){
-  var $ = require('cheerio').load(body, { xmlMode: true, ignoreWhitespace: false, lowerCaseTags: false });
-  var json = { tts : 'Météo: '};
+
+  var meteo  = JSON.parse(body).result;
+  var json   = { tts : 'Météo: '};
+  var prev   = getPrevision(meteo, date);
+
+  json.place = meteo.ville.nom;
+  json.temp  = prev.temperatureMin + '°C';
+  json.sun   = prev.description;
   
-  json.place = $('#prevision > H2').text();      // Place
   json.tts  += json.place + ": ";
+  json.tts  += day[prev.jour+'_'+prev.moment]+', ';
+  json.tts  += json.sun + ', '
   
-  var tr = $('#prevision table.prevSem1 tr');
-  var td = $(tr[date]).find('td');
-  
-  json.temp = $(td[2]).text()                    // Temperature
-  json.sun  = $(td[1]).find('img').attr('alt')   // Sun
-  
-  json.tts += $(td[0]).text() + ', ';            // Days 
-  json.tts += json.sun + ', ' + json.temp + ', ';
+  if (prev.temperatureMin != prev.temperatureMax){
+    json.tts  += 'température entre ' + prev.temperatureMax + ' et ' + prev.temperatureMin + ' degrés';
+  } else {
+    json.tts  += 'température de ' + prev.temperatureMin + ' degrés';
+  } 
  
   return json;
+}
+
+var getPrevision = function(meteo, date){
+  var prevision = meteo.previsions;
+  
+  if (date.length > 2){
+    return prevision[date]; 
+  }
+  
+  date = parseInt(date);
+  for (var p in prevision){ if (date-- <= 0) return prevision[p];  }
+}
+
+var day = {
+  '0_matin': 'ce matin', 
+  '0_midi' : 'ce midi', 
+  '0_soir' : 'ce soir', 
+  '0_nuit' : 'cette nuit',
+  '1_matin': 'demain matin', 
+  '1_midi' : 'demain midi', 
+  '1_soir' : 'demain soir', 
+  '1_nuit' : 'la nuit prochaine',
+  '2_matin': 'après demain matin', 
+  '2_midi' : 'après demain midi', 
+  '2_soir' : 'après demain soir', 
+  '2_nuit' : 'dans 2 nuits'
 }
